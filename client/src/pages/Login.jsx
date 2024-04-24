d7187be7ffd933889c542cf9dd289ea7fc3cafac
Pilag6 <pilag6@gmail.com>
2024-04-24 10:59:14 +0200

    Add react-icons package to client dependencies

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,6 +3,13 @@ import Axios from "axios";
 // UseNavigate
 import { useNavigate } from "react-router-dom";
 
+// Assets
+import logoWhite from "../assets/logo-white.png";
+
+// Icons
+import { PiLockKey } from "react-icons/pi";
+import { MdAlternateEmail } from "react-icons/md";
+
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -41,14 +48,21 @@ const Login = () => {
             </div>
 
             <div className="flex h-screen flex-1 flex-col justify-center">
-                <h2 className="text-center text-4xl font-semibold mb-10">
+                {/* <h2 className="text-center text-4xl font-semibold mb-10">
                     Login
-                </h2>
+                </h2> */}
+                <img
+                    className="flex justify-center items-center mx-auto "
+                    src={logoWhite}
+                    alt=""
+                    width={180}
+                />
                 <form
                     onSubmit={handleLogin}
                     className="mx-auto flex w-2/4 flex-col items-center justify-center"
                 >
                     <div className="mb-2 flex w-full items-center gap-3 border-b-2 border-cyan-50/45 px-4 py-4 text-white relative">
+                        <MdAlternateEmail />
                         <input
                             className="w-full bg-transparent text-white outline-none"
                             type="email"
@@ -57,9 +71,9 @@ const Login = () => {
                             onChange={(e) => setEmail(e.target.value)}
                             placeholder="Email"
                         />
-
                     </div>
                     <div className="mb-2 flex w-full items-center gap-3 border-b-2 border-cyan-50/45 px-4 py-4 text-white">
+                        <PiLockKey />
                         <input
                             type="password"
                             id="password"
@@ -72,9 +86,9 @@ const Login = () => {
                     <button className="mt-7 w-full py-4 text-2xl font-semibold text-white outline-none outline-white transition-all hover:bg-[#B3C4A2] hover:text-zinc-800 hover:outline-2">
                         Login
                     </button>
-                    <div>
+                    <div className="w-full outline-white">
                         {error && (
-                            <p className="mt-7 w-full py-4 px-6 text-2xl font-semibold text-white transition-all bg-red-400 hover:text-zinc-800 hover:outline-2">
+                            <p className="mt-7 w-full py-4 px-6 text-2xl font-semibold text-white transition-all bg-red-400">
                                 {error}
                             </p>
                         )}
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,6 +3,14 @@ import Axios from "axios";
 // UseNavigate
 import { useNavigate } from "react-router-dom";
 
+// Assets
+import logoWhite from "../assets/logo-white.png";
+
+// Icons
+import { FaUserAstronaut } from "react-icons/fa6";
+import { MdAlternateEmail } from "react-icons/md";
+import { PiLockKey } from "react-icons/pi";
+
 const Register = () => {
     // UseNavigate
     const navigate = useNavigate();
@@ -32,44 +40,69 @@ const Register = () => {
     };
 
     return (
-        <>
-            <h1>Register</h1>
-            <form onSubmit={handleRegister}>
-                <div>
-                    <label htmlFor="username">Username</label>
-                    <br />
-                    <input
-                        type="text"
-                        id="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <br />
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <br />
-                    <input
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <button type="submit">Register</button>
-            </form>
+        <div className="mx-auto flex h-screen bg-zinc-800">
+            <div className="h-screen flex-1">
+                <img
+                    className="h-screen object-cover object-center"
+                    src="https://bit.ly/4aPzJfY"
+                    alt=""
+                />
+            </div>
+            <div className="flex h-screen flex-1 flex-col justify-center">
+                <img
+                    className="flex justify-center items-center mx-auto "
+                    src={logoWhite}
+                    alt=""
+                    width={180}
+                />
+                <form
+                    onSubmit={handleRegister}
+                    className="mx-auto flex w-2/4 flex-col items-center justify-center"
+                >
+                    <div className="mb-2 flex w-full items-center gap-3 border-b-2 border-cyan-50/45 px-4 py-4 text-white">
+                        <FaUserAstronaut />
+                        <input
+                            className="w-full bg-transparent text-white outline-none"
+                            type="text"
+                            id="username"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-2 flex w-full items-center gap-3 border-b-2 border-cyan-50/45 px-4 py-4 text-white relative">
+                        <MdAlternateEmail />
+                        <input
+                            className="w-full bg-transparent text-white outline-none"
+                            type="email"
+                            id="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-2 flex w-full items-center gap-3 border-b-2 border-cyan-50/45 px-4 py-4 text-white">
+                        <PiLockKey />
+                        <input
+                            className="w-full bg-transparent text-white outline-none"
+                            type="password"
+                            id="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="mt-7 w-full py-4 text-2xl font-semibold text-white outline-none outline-white transition-all hover:bg-[#B3C4A2] hover:text-zinc-800 hover:outline-2"
+                    >
+                        Register
+                    </button>
+                </form>
+            </div>
 
             {/* {error && <p>{error}</p>} */}
-        </>
+        </div>
     );
 };
 export default Register;
