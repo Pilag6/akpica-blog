2d6a4d9c93f6cdbe997e3e6b05d3a571c4e42cca
Pilag6 <pilag6@gmail.com>
2024-04-24 11:50:58 +0200

    Refactor DHAdmin.jsx to use Tailwind CSS classes for styling

--- client/src/pages/DHAdmin.jsx
+++ client/src/pages/DHAdmin.jsx
@@ -1,10 +1,19 @@
 import { NavLink } from "react-router-dom";
 
+const BtnClasses = "mt-7 py-4 text-2xl w-80 font-semibold text-white outline-none outline-white transition-all hover:bg-[#B3C4A2] hover:text-zinc-800 hover:outline-2 text-center";
+
 const DHAdmin = () => {
     return (
-        <div>
-            <NavLink to="/dh-admin/register">Register</NavLink><br />
-            <NavLink to="/dh-admin/login">Login</NavLink>
+        <div className="h-screen flex justify-center w-80 items-center gap-8 w-3/4 mx-auto">
+  
+            <NavLink to="/dh-admin/register" className={BtnClasses}>
+                Register
+            </NavLink>
+            <NavLink to="/dh-admin/login" className={BtnClasses}>
+                Login
+            </NavLink>
+
+
         </div>
     );
 };
