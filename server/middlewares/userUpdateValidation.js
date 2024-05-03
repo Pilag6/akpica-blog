99343c1c5df95f4211d5a294df89e1cb40c22247
akiko-luka <ak.luka051@gmail.com>
2024-05-03 11:17:43 +0200

    chore: start user update validation middleware

---
+++ server/middlewares/userUpdateValidation.js
@@ -0,0 +1,97 @@
+import { body, validationResult } from "express-validator";
+import UserModel from "../models/userModel.js";
+
+const userUpdateValidation = () => {
+  return [
+    body("email")
+            .exists()
+            .withMessage("You have to enter an email")
+            .notEmpty()
+            .withMessage("Your email is empty")
+            .isEmail()
+            .withMessage("Enter a valid email")
+            .custom(async (value) => {
+                const user = await UserModel.findOne({ email: value });
+                if (user) {
+                    return Promise.reject("Email already in use");
+                }
+            })
+            .withMessage("Email already in use")
+            .toLowerCase()
+            .escape(),
+        body("password")
+            .exists()
+            .withMessage("You have to enter a password")
+            .notEmpty()
+            .withMessage("Your password is empty")
+            .isString()
+            .withMessage("Password should be a string")
+            .trim()
+            .isStrongPassword({
+                minLength: 5,
+                maxLength: 15,
+                minLowercase: 1,
+                minUppercase: 1,
+                minNumbers: 1,
+                minSymbols: 1
+            })
+            .withMessage(
+                "Password must contain at least one lowercase letter, one uppercase letter, one number, and one special character and must be between 5 to 15 characters"
+            )
+            .custom((value, req) => {
+                const { username, email } = req.req.body;
+                // console.log("REQUEST", email.substring(0, email.indexOf("@")));
+
+                const emailSubs = email.substring(0, email.indexOf("@"));
+
+                // Should not contain the username or parts of the user’s email.
+
+                if (
+                    value.toLowerCase().slice(0, username.length) !==
+                        username.toLowerCase() &&
+                    value.toLowerCase().slice(0, emailSubs.length) !== emailSubs
+                ) {
+                    // console.log(value.toLowerCase().slice(0, emailSubs.length -1));
+                    return true;
+                }
+                throw new Error(
+                    "Password should not contain the username or parts of the user’s email"
+                );
+            }),
+        body("fullname")
+            .optional()
+            .isString()
+            .withMessage("Fullname should be a string")
+            .trim()
+            .isLength({ min: 3, max: 30 })
+            .withMessage("Fullname must be between 3 to 30 characters")
+            .not()
+            .matches(/(<([^>]+)>)/gi)
+            .toLowerCase()
+            .escape(),
+    ];
+};
+
+const userUpdateValidate = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    const { email, password, fullname } = req.body;
+    const userUpdate = UserModel.updateOne(
+      { email, password, fullname })
+    if (userUpdate) {
+      return res.status(200).json({
+        message: "User updated",
+      });
+    }
+      return next();
+  }
+
+  const extractedErrors = [];
+  errors.array().map((err) => extractedErrors.push({ [err.path]: err.msg }));
+
+  return res.status(400).json({
+    errors: extractedErrors
+  })
+}
+
+export { userUpdateValidation, userUpdateValidate}
