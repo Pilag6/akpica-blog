489ce448de1677466221649115c4d4b4da7ef552
akiko-luka <ak.luka051@gmail.com>
2024-05-06 11:22:16 +0200

    chore: Add z-index to DashboardLayout header

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -5,7 +5,7 @@ import NavbarDashboard from "@components/DashboardStructure/NavbarDashboard.jsx"
 const DashboardLayout = ({ children }) => {
     return (
         <>
-            <header className="fixed top-0 left-0 w-full h-20 bg-akpica-green py-4 px-5 flex items-center">
+            <header className="fixed top-0 left-0 w-full h-20 bg-akpica-green py-4 px-5 flex items-center z-10">
                 <NavbarDashboard />
             </header>
             <aside className="fixed top-[80px] left-0 w-[200px] bg-akpica-carlo h-[calc(100vh-80px)] py-4 px-5 flex pb-2">
