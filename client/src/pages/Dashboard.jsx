17790063b391d0abde2349c3a3a50c5a9f844e68
akiko-luka <ak.luka051@gmail.com>
2024-05-06 10:14:51 +0200

    chore: Update Dashboard layout with centered logo

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,9 +5,9 @@ import Logo from "@assets/logo-white.png";
 const Dashboard = () => {
     return (
         <>
-            <div className="flex flex-col justify-center gap-8 w-full">
+            <div className="flex flex-col justify-center items-center gap-8 w-full pt-8">
                 <h2 className="text-3xl">Dashboard</h2>
-                <img className="w-48" src={Logo} alt="" />
+                <img className="" src={Logo} alt="" />
             </div>
         </>
     );
