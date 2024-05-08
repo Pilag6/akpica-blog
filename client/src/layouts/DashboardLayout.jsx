459b0a7c49b9ccfda4f01ffb30acd0bc22722b10
Pilag6 <pilag6@gmail.com>
2024-05-08 11:31:46 +0200

    Update user profile component to handle invalid password and display error message

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -8,7 +8,7 @@ const DashboardLayout = ({ children }) => {
             <header className="fixed top-0 left-0 w-full h-20 bg-akpica-green py-4 px-5 flex items-center z-10">
                 <NavbarDashboard />
             </header>
-            <aside className="fixed top-[80px] left-0 w-[200px] bg-akpica-carlo h-[calc(100vh-80px)] py-4 px-5 flex pb-2">
+            <aside className="fixed top-[80px] left-0 w-[200px] bg-akpica-carlo h-[calc(100vh-80px)] py-4 px-5 flex">
                 <AsideDashboard />
             </aside>
             <main className="pl-[200px] mt-[5rem] w-full">{children}</main>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,12 +4,12 @@ import Logo from "@assets/logo-white.png";
 
 const Dashboard = () => {
     return (
-        <>
-            <div className="flex flex-col justify-center items-center gap-8 w-full pt-8">
-                <h2 className="text-3xl">Dashboard</h2>
-                <img className="" src={Logo} alt="" />
+        
+            <div className="flex flex-col justify-center items-center w-full h-[calc(100vh-80px)]">
+                <img className="w-72" src={Logo} alt="" />
+                <h2 className="py-4 px-20 text-2xl font-semibold text-akpica-white outline-none outline-white transition-all hover:bg-akpica-pastel hover:text-zinc-800 hover:outline-2">Dashboard</h2>
             </div>
-        </>
+    
     );
 };
 export default Dashboard;
