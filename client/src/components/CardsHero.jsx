2f2ee69c153fdbac488146a84a3ecd79c2c3a5e3
Pilag6 <pilag6@gmail.com>
2024-05-15 10:25:49 +0200

    feat: Add CardsHero, Hero, and Navbar components

---
+++ client/src/components/CardsHero.jsx
@@ -0,0 +1,16 @@
+const CardsHero = () => {
+    return (
+        <div
+            className="w-full h-[65vh]"
+            style={{
+                backgroundImage: "url('https://bit.ly/44ItnwG')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat"
+            }}
+        >
+            CardsHero
+        </div>
+    );
+};
+export default CardsHero;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,39 +1,22 @@
-import { Link } from "react-router-dom";
-import Logo from "@assets/logo-white.png";
-import { useContext } from "react";
-import { PostContext } from "@contexts/PostContext.jsx";
+// import { Link } from "react-router-dom";
+// import Logo from "@assets/logo-white.png";
+// import { useContext } from "react";
+// import { PostContext } from "@contexts/PostContext.jsx";
+
+import CardsHero from "@components/CardsHero.jsx";
+import Hero from "@components/Hero.jsx";
+import Navbar from "@components/Navbar.jsx";
 
 const Home = () => {
-    const { posts } = useContext(PostContext);
+    // const { posts } = useContext(PostContext);
 
     return (
         <>
-            <div className="flex flex-col justify-center items-center  bg-akpica-carlo">
-                <img className="w-72" src={Logo} alt="" />
-                <div className="mb-12 -mt-10 text-2xl font-semibold">
-                    Website Under Construction
-                </div>
-                <Link
-                    className="py-4 px-8 text-2xl font-semibold text-akpica-white outline-none outline-white transition-all hover:bg-akpica-pastel hover:text-zinc-800 hover:outline-2"
-                    to="/dh-admin"
-                >
-                    Go to Dashboard
-                </Link>
+            <div className="bg-akpica-white min-h-screen">
+                <Navbar />
+                <Hero />
+                <CardsHero />
             </div>
-
-            {posts &&
-                posts.map((post) => (
-                    <div
-                        key={post._id}
-                        className="flex flex-col justify-center items-center bg-akpica-carlo border-2 mt-8"
-                    >
-                        <div className="text-2xl font-semibold">
-                            {post.title}
-                        </div>
-                        <div>{post.author.username}</div>
-                        <div className="text-xl">{post.content}</div>
-                    </div>
-                ))}
         </>
     );
 };
