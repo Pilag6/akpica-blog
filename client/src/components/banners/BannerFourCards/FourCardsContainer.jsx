b5fe6764cf6cc5e6f208d1094711eaf3aaf5bd6c
akiko-luka <ak.luka051@gmail.com>
2024-05-22 15:14:40 +0200

    refactor: import posts from Backend

--- client/src/components/banners/BannerFourCards/FourCardsContainer.jsx
+++ client/src/components/banners/BannerFourCards/FourCardsContainer.jsx
@@ -7,24 +7,27 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import akiko from "../../../../../server/uploads/akiko.jpg";
 import carlos from "../../../../../server/uploads/carlos.jpg";
 import pila from "../../../../../server/uploads/Pila.jpg";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { PostContext } from "@contexts/PostContext.jsx";
 
 // ({ bgImage, category, title, author, avatar, date }) => {
 
 const FourCardsContainer = () => {
+  let { posts } = useContext(PostContext);
   const [arrow, setArrow] = useState(0);
 
+  // START arrow
   const handlePrevSlide = () => {
     setArrow((prevArrow) =>
       prevArrow === 0 ? imageSlider.length - 1 : prevArrow - 1
     );
   };
-
   const handleNextSlide = () => {
     setArrow((prevArrow) =>
       prevArrow === imageSlider.length - 1 ? 0 : prevArrow + 1
     );
   };
+  // END arrow
 
   return (
     // <div className="w-full mx-auto flex my-11 group relative">
@@ -45,7 +48,21 @@ const FourCardsContainer = () => {
         </button>
       </div>
 
-      <FourCards
+      {posts && posts.slice(0, 4).map((card) => (
+        <FourCards key={card._id}
+        bgImage={`http://localhost:3300/posts/photo/${encodeURIComponent(
+          card.title
+      )}?${new Date().getTime()}`}
+      category={card.tags}
+      title={card.title}
+      avatar={`http://localhost:3300/photo/${
+        card.author.username
+    }?${new Date().getTime()}`}
+    author={card.author.username} 
+    date={new Date(card.date).toDateString()}/>
+      ))}
+
+      {/* <FourCards
         bgImage={"https://bit.ly/3QLDXNI"}
         category={"REACT"}
         title={"React Starter Blog"}
@@ -79,7 +96,7 @@ const FourCardsContainer = () => {
         avatar={pila}
         author={"Pila"}
         date={"May 16, 2024"}
-      />
+      /> */}
     </div>
   );
 };
