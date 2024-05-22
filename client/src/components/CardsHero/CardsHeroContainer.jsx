0f3124061638c5eeabba006e178c87aedcc007f6
Pilag6 <pilag6@gmail.com>
2024-05-22 14:42:00 +0200

    refactor: Update CardTag component to use uppercase for tag text

--- client/src/components/CardsHero/CardsHeroContainer.jsx
+++ client/src/components/CardsHero/CardsHeroContainer.jsx
@@ -1,53 +1,29 @@
 import CardHero from "./CardHero.jsx";
 
-import bunReact from "@assets/bun-react.webp";
-import tailwindImg from "@assets/tailwind.webp";
-import astroImg from "@assets/astro.webp";
+import { useContext } from "react";
+import { PostContext } from "@contexts/PostContext.jsx";
 
-import akiko from "../../../../server/uploads/akiko.jpg";
-import carlos from "../../../../server/uploads/carlos.jpg";
-import pila from "../../../../server/uploads/Pila.jpg";
-
-const cardData = [
-    {
-        bgImg: bunReact,
-        tagName: "REACT",
-        title: "Getting Started with Bun for React Developers",
-        author: "Carlos",
-        avatar: carlos,
-        date: "May 23, 2024",
-    },
-    {
-        bgImg: tailwindImg,
-        tagName: "TAILWIND",
-        title: "Build a Blog App Project Using Tailwind CSS",
-        author: "Akiko",
-        avatar: akiko,
-        date: "May 13, 2024",
-    },
-    {
-        bgImg: astroImg,
-        tagName: "ASTRO",
-        title: "Building a simple but scalable blog using Astro",
-        author: "Pila",
-        avatar: pila,
-        date: "May 16, 2024",
-    },
-];
 
 const CardsHero = () => {
+
+    let { posts } = useContext(PostContext);
+    // console.log(posts);
     return (
         <div className="relative w-full -mt-20 z-20">
             <div className="max-w-[1200px] mx-auto flex justify-center items-center flex-wrap gap-6">
-                {cardData.map((card, index) => (
+                {posts && posts.slice(0,3).map((card) => (
                     <CardHero
-                        key={index}
-                        bgImg={card.bgImg}
-                        tagName={card.tagName}
+                        key={card._id}
+                        bgImg={`http://localhost:3300/posts/photo/${encodeURIComponent(
+                            card.title
+                        )}?${new Date().getTime()}`}
+                        tagName={card.tags}
                         title={card.title}
-                        author={card.author}
-                        avatar={card.avatar}
-                        date={card.date}
+                        author={card.author.username}
+                        avatar={`http://localhost:3300/photo/${
+                            card.author.username
+                        }?${new Date().getTime()}`}
+                        date={new Date(card.date).toDateString()}
                         colorTag="bg-akpica-tomato"
                         sizeTag="text-sm"
                         width="w-96"
