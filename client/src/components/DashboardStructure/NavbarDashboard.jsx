c77e12be09c9e5857e67f96ef514bb5a0ba9c72b
Pilag6 <pilag6@gmail.com>
2024-05-23 14:47:30 +0200

    refactor: Update NavbarDashboard component to include link to homepage

--- client/src/components/DashboardStructure/NavbarDashboard.jsx
+++ client/src/components/DashboardStructure/NavbarDashboard.jsx
@@ -1,9 +1,10 @@
 import LogoBlackH from '@assets/logo-black-h.png'
+import { Link } from 'react-router-dom'
 
 const NavbarDashboard = () => {
   return (
     <div>
-      <div><img className='h-20' src={LogoBlackH} alt=""/></div>
+      <Link to={"/"}><img className='h-20' src={LogoBlackH} alt=""/></Link>
     </div>
   )
 }
