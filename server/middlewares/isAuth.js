05ebbf339c8307129ce0960e607fcc19bba87e13
Pilag6 <pilag6@gmail.com>
2024-05-27 11:34:11 +0200

    refactor: Improve token handling in axiosInstance and isAuth middleware

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -5,6 +5,8 @@ const { JWT_SECRET } = process.env;
 const isAuth = asyncHandler(async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
 
+  console.log('Token:', token);  // Add this log to check if the token is received
+
   if (token) {
     try {
       const payload = jwt.verify(token, JWT_SECRET);
@@ -12,10 +14,12 @@ const isAuth = asyncHandler(async (req, res, next) => {
       next();
     } catch (error) {
       res.status(401);
+      console.error("Token verification failed:", error);  // Log verification errors
       throw new Error("Not Authorized, invalid token");
     }
   } else {
     res.status(401);
+    console.error("No token provided");  // Log missing token errors
     throw new Error("Not Authorized, no token");
   }
 });
