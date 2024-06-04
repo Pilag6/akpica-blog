225899e71e67e178d246160b5bd0a79e18307242
Pilag6 <pilag6@gmail.com>
2024-06-04 15:57:10 +0200

    It is NONE!!!!

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -75,7 +75,7 @@ const login = asyncHandler(async (req, res) => {
         res.cookie("token", accessToken, {
             httpOnly: true,
             secure: true,
-            sameSite: "strict",
+            sameSite: "none",
         });
 
         res.status(200).json({
