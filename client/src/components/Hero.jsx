8f521bd3de322d67e014b19dc7a6e9d4f2c59392
akiko-luka <ak.luka051@gmail.com>
2024-06-11 09:31:04 +0200

    refactor: Add bgColor prop to AuthorDate component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -61,72 +61,71 @@ const Hero = () => {
     }, [limitedPosts]);
 
     return (
-        <div className="relative w-full h-[65vh] z-10 group">
-            {limitedPosts.map((post, index) => (
-                <div
-                    key={post._id}
-                    className={`absolute w-full h-full transition-opacity duration-[1000ms] ${
-                        index === currentIndex
-                            ? "opacity-100 z-50"
-                            : "opacity-0"
-                    }`}
-                    style={{
-                        backgroundImage: `url(${BACKEND_URL}/posts/photo/${encodeURIComponent(
-                            post.title
-                        )})`,
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
-                        backgroundRepeat: "no-repeat"
-                    }}
-                >
-                    <div className="flex flex-col justify-center h-full max-w-[1200px] mx-auto p-6 gap-2">
-                        <CardTag
-                            tag={post.tags[0]}
-                            color="bg-akpica-marco text-akpica-black"
-                            size="md:text-md text-sm"
-                            link={`/tags/${post.tags}`}
-                            linkTag={`/tags/${post.tags[0]}`}
-                        />
-                        <Link to={`${post._id}`} className="md:w-1/2 p-2">
-                            <h1 className="w-fit decoration-clone md:text-5xl text-3xl bg-akpica-white px-5 md:leading-snug leading-snug inline uppercase font-[700] font-akpica-heading text-akpica-black">
-                                {post.title}
-                            </h1>
-                        </Link>
-                        <AvatarDate
-                            avatar={`${BACKEND_URL}/photo/${post.author.username}`}
-                            author={post.author.username}
-                            date={new Date(post.date).toDateString()}
-                            colors="text-akpica-white"
-                        />
-                    </div>
-                </div>
-            ))}
-            <button
-                className="absolute top-1/2 left-0 md:m-5 transform -translate-y-1/2 bg-akpica-marco text-akpica-carlo px-2 py-4 opacity-0 group-hover:opacity-100 group-hover:z-50 transition-opacity duration-300"
-                onClick={prevSlide}
-            >
-                <FaChevronLeft />
-            </button>
-            <button
-                className="absolute top-1/2 right-0 md:m-5 transform -translate-y-1/2 bg-akpica-marco text-akpica-carlo px-2 py-4 opacity-0 group-hover:opacity-100 group-hover:z-50 transition-opacity duration-300"
-                onClick={nextSlide}
-            >
-                <FaChevronRight />
-            </button>
+      <div className="relative w-full h-[65vh] z-10 group">
+        {limitedPosts.map((post, index) => (
+          <div
+            key={post._id}
+            className={`absolute w-full h-full transition-opacity duration-[1000ms] ${
+              index === currentIndex ? "opacity-100 z-50" : "opacity-0"
+            }`}
+            style={{
+              backgroundImage: `url(${BACKEND_URL}/posts/photo/${encodeURIComponent(
+                post.title
+              )})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            <div className="flex flex-col justify-center h-full max-w-[1200px] mx-auto p-6 gap-2">
+              <CardTag
+                tag={post.tags[0]}
+                color="bg-akpica-marco text-akpica-black"
+                size="md:text-md text-sm"
+                link={`/tags/${post.tags}`}
+                linkTag={`/tags/${post.tags[0]}`}
+              />
+              <Link to={`${post._id}`} className="md:w-1/2 p-2">
+                <h1 className="w-fit decoration-clone md:text-5xl text-3xl bg-akpica-white px-5 md:leading-snug leading-snug inline uppercase font-[700] font-akpica-heading text-akpica-black">
+                  {post.title}
+                </h1>
+              </Link>
+              <AvatarDate
+                avatar={`${BACKEND_URL}/photo/${post.author.username}`}
+                author={post.author.username}
+                date={new Date(post.date).toDateString()}
+                colors="text-akpica-white"
+                bgColor="bg-akpica-black/60 w-fit p-2"
+              />
+            </div>
+          </div>
+        ))}
+        <button
+          className="absolute top-1/2 left-0 md:m-5 transform -translate-y-1/2 bg-akpica-marco text-akpica-carlo px-2 py-4 opacity-0 group-hover:opacity-100 group-hover:z-50 transition-opacity duration-300"
+          onClick={prevSlide}
+        >
+          <FaChevronLeft />
+        </button>
+        <button
+          className="absolute top-1/2 right-0 md:m-5 transform -translate-y-1/2 bg-akpica-marco text-akpica-carlo px-2 py-4 opacity-0 group-hover:opacity-100 group-hover:z-50 transition-opacity duration-300"
+          onClick={nextSlide}
+        >
+          <FaChevronRight />
+        </button>
 
-            <button
-                className="absolute top-1/2 left-0 md:m-5 transform -translate-y-1/2 bg-akpica-marco text-akpica-carlo px-2 py-4 opacity-0 group-hover:opacity-100 group-hover:z-50 transition-opacity duration-300"
-                onClick={prevSlide}
-            >
-                <FaChevronLeft />
-            </button>
-            <button
-                className="absolute top-1/2 right-0 md:m-5 transform -translate-y-1/2 bg-akpica-marco text-akpica-carlo px-2 py-4 opacity-0 group-hover:opacity-100 group-hover:z-50 transition-opacity duration-300"
-                onClick={nextSlide}
-            >
-                <FaChevronRight />
-            </button>
-        </div>
+        <button
+          className="absolute top-1/2 left-0 md:m-5 transform -translate-y-1/2 bg-akpica-marco text-akpica-carlo px-2 py-4 opacity-0 group-hover:opacity-100 group-hover:z-50 transition-opacity duration-300"
+          onClick={prevSlide}
+        >
+          <FaChevronLeft />
+        </button>
+        <button
+          className="absolute top-1/2 right-0 md:m-5 transform -translate-y-1/2 bg-akpica-marco text-akpica-carlo px-2 py-4 opacity-0 group-hover:opacity-100 group-hover:z-50 transition-opacity duration-300"
+          onClick={nextSlide}
+        >
+          <FaChevronRight />
+        </button>
+      </div>
     );
 };
 
