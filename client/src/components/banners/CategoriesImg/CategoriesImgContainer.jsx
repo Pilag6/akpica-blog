1eb4fbaf43cced8b280a32587dc5cfa244211e07
Pilag6 <pilag6@gmail.com>
2024-06-11 15:21:14 +0200

    refactor: Update category tags in CategoriesImgContainer

--- client/src/components/banners/CategoriesImg/CategoriesImgContainer.jsx
+++ client/src/components/banners/CategoriesImg/CategoriesImgContainer.jsx
@@ -30,14 +30,14 @@ const CategoriesImgContainer = () => {
 
     const uniqueTags = new Set();
     const categories = [
-        { image: category1, tag: getTag(18) },
-        { image: category2, tag: getTag(1) },
-        { image: category3, tag: getTag(2) },
-        { image: category6, tag: getTag(3) },
-        { image: category5, tag: getTag(9) },
-        { image: category4, tag: getTag(5) },
-        { image: category7, tag: getTag(6) },
-        { image: category8, tag: getTag(10) }
+        { image: category1, tag: "node" },
+        { image: category2, tag: "react" },
+        { image: category3, tag: "javascript" },
+        { image: category6, tag: "express" },
+        { image: category5, tag: "mondodb" },
+        { image: category4, tag: "uxui design" },
+        { image: category7, tag: "frontend" },
+        { image: category8, tag: "backend" }
     ].filter(category => {
         if (category.tag && !uniqueTags.has(category.tag)) {
             uniqueTags.add(category.tag);
